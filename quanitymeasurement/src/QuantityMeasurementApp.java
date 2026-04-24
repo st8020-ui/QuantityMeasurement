ec13d3a6db047c98fceb4f31e130c7df1fd3bf63
st8020-ui <st8020@srmist.edu.in>
2026-04-24 08:38:17 +0530

    Quantity measurement app development start

---
+++ quanitymeasurement/src/QuantityMeasurementApp.java
@@ -0,0 +1,7 @@
+public class QuantityMeasurementApp {
+    static void main(String[] args) {
+        System.out.println("Quantity Measuement App development ");
+    }
+}
+    // Inner class Feet
+
